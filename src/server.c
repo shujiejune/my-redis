af72857774346b236a0c948b37e2d34840c132fd
shujiejune <jieniu.june@tutanota.com>
2026-01-16 11:29:11 -0800

    handle multiple requests in a single connection

--- src/server.c
+++ src/server.c
@@ -2,10 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <assert.h>
+#include <errno.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+const size_t k_max_msg = 4096;
+
+static int32_t read_full(int fd, char *buf, size_t n);
+static int32_t write_all(int fd, char *buf, size_t n);
+static int32_t one_request(int conn_fd);
 static void do_something(int conn_fd);
+static void msg(const char *msg);
+static void die(const char *msg);
 
 int main() {
     /* 1. Obtain a socket handle */
@@ -35,14 +44,12 @@ int main() {
     addr.sin_addr.s_addr = htonl(0);  // 0.0.0.0
     int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
     if (rv) {
-        perror("bind failed");
-        exit(1);
+        die("bind failed");
     }
 
     /* 4. Listen */
     if (listen(fd, SOMAXCONN) < 0) {
-        perror("listen failed");
-        exit(1);
+        die("listen failed");
     }
     printf("Server listening on port 6379...\n");
 
@@ -71,15 +78,108 @@ int main() {
     return 0;
 }
 
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        // size_t is non-negative (0 - MAX_INT), used for counting.
+        // ssize_t is signed, used when a function needs to return a count or an error.
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error or unexpected EOF
+        }
+        // sanity check for things that should be impossible.
+        // If read() returns MORE bytes than n, crash immediately.
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+/* Implementation of a Length-Prefixed Framing Protocol */
+static int32_t one_request(int conn_fd) {
+    /* 1. Create the read buffer */
+    char rbuf[4 + k_max_msg];  // 4 bytes header
+
+    /* 2. Read exactly 4 bytes (the header) */
+    // errno is a Thread Local Gloabl Variable
+    // If the kernel fails, it returns -1 to the C program and
+    // writes the specific error code into the errno variable.
+    errno = 0;
+    int32_t err = read_full(conn_fd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    /* 3. Interpret the header as the length of the message */
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    /* 4. Ready exactly len bytes (the body) */
+    err = read_full(conn_fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    /* 5. Print the message */
+    // print a string of a specific length
+    printf("client says: %.*s\n", len, &rbuf[4]);
+
+    /* 6. Prepare response */
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+
+    /* 7. Write leangth header and body to the write buffer */
+    len = (uint32_t)strlen(reply);
+    memcpy(rbuf, &len, 4);
+    memcpy(&rbuf[4], reply, len);
+
+    /* 8. Send the repsonse all at once */
+    return write_all(conn_fd, wbuf, 4 + len);
+}
+
 static void do_something(int conn_fd) {
     char rbuf[64] = {};
     ssize_t n = read(conn_fd, rbuf, sizeof(rbuf) - 1);  // leave the 64th byte as a guaranteed zero
     if (n < 0) {
-        perror("Read failed");
+        msg("Read failed");
         return;
     }
-    printf("Client says: %s\n", rbuf);
+    fprintf(stderr, "Client says: %s\n", rbuf);
 
     char wbuf[] = "world";
     write(conn_fd, wbuf, strlen(wbuf));
+}
+
+static void msg(const char *msg) {
+    // printf is buffered, writes to stdout, i.e. the result of the program.
+    // fprintf is unbuffered (appears immediately), writes to stderr, separated from the data stream.
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    // exit(1): quit. It cleans up, flushes buffers, and closes files gracefully.
+    // abort(): crash. It raises the SIGABRT signal, forces the OS to dump a
+    // snapshot of memory at the crashing moment, which can be opened in GDB later.
+    abort();
 }
