86d5636cb4f398055255e96bd64bcd56b7dd8774
shujiejune <jieniu.june@tutanota.com>
2026-02-08 00:00:07 -0800

    client for event loop

--- src/client.c
+++ src/client.c
@@ -7,9 +7,11 @@
 #include <netinet/in.h>
 #include "common.h"
 
-const size_t k_max_msg = 4096;
+#define k_max_msg 4096
 
 static int32_t query(int fd, const char *text);
+static int32_t send_req(int fd, const char *text);
+static int32_t read_res(int fd);
 
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -26,17 +28,71 @@ int main() {
         die("Connect failed");
     }
 
-    int32_t err = query(fd, "hello1");
-    if (err) {
-        goto L_DONE;
+    // Multiple pipelined requests
+    char *long_msg = malloc(k_max_msg + 1);
+    if (!long_msg) {
+        die("malloc failed");
     }
-    err = query(fd, "hello2");
-    if (err) {
-        goto L_DONE;
+    memset(long_msg, 'z', k_max_msg);
+    long_msg[k_max_msg] = '\0';
+
+    const char *query_list[] = {"hello1", "hello2", "pipelined", long_msg, "test"};
+    size_t query_count = 5;
+
+    // --- PIPELINING STEP 1: SEND EVERYTHING ---
+    printf("--- Sending %d requests ---\n", (int)query_count);
+    for (int i = 0; i < query_count; i++) {
+        int32_t err = send_req(fd, query_list[i]);
+        if (err) goto L_DONE;
+    }
+
+    // --- PIPELINING STEP 2: READ EVERYTHING ---
+    printf("--- Waiting for responses ---\n");
+    for (int i = 0; i < query_count; i++) {
+        int32_t err = read_res(fd);
+        if (err) goto L_DONE;
     }
 
 L_DONE:
     close(fd);
+    return 0;
+}
+
+static int32_t send_req(int fd, const char *text) {
+    uint32_t len = (uint32_t)strlen(text);
+    if (len > k_max_msg) {
+        return-1;
+    }
+
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    return write_all(fd, wbuf, 4 + len);
+}
+
+static int32_t read_res(int fd) {
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg) {
+        msg("response too long");
+        return -1;
+    }
+
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    printf("Server says: %.*s\n", len, &rbuf[4]);
     return 0;
 }
 
